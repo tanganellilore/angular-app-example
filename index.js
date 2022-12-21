1887d6210142c0ad48f3b0bd8319d24f855dac67
lorenzo.tanganelli <lorenzo.tanganelli@staff.aruba.it>
2022-12-21 17:35:12 +0100

    add https

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const app_t = require('./frontend/dist/angular-starter/server/main.js').app();
 app_t.enable('trust proxy');
+app_t.use((req, res, next) => {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 module.exports = app_t;
